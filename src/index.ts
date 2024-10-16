700cc2c7dec052c21202f170811c1f324142df59
Infant Ajay Venus <infantcsajay@outlook.com>
2024-10-16 19:48:00 +0530

    [index] patch: update add to return the sum of numbers

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 export function add(numbers: string): number {
     if(numbers === "")  return 0;
-    return -1;
+
+    return numbers
+        .split(",")
+        .map((number) => parseInt(number))
+        .reduce((sum, number) => sum + number, 0);
 }
