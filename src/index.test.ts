e2ca87e1e16e4add5f0f935266722fc21559fcb3
Infant Ajay Venus <infantcsajay@outlook.com>
2024-10-16 19:25:59 +0530

    [index] patch: update add to

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,4 +4,8 @@ describe("add", () => {
     it("should be called", () => {
         expect(add).toBeDefined();
     });
+
+    it("should return 0 when empty string is passed" , () => {
+        expect(add("")).toBe(0);
+    });
 });
