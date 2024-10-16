e2b0745759c1e184206b47798806380de24f7a03
Infant Ajay Venus <infantcsajay@outlook.com>
2024-10-16 19:46:01 +0530

    [test] patch: update test suite to include base test case
    - return sum of positive numbers

--- src/index.test.ts
+++ src/index.test.ts
@@ -8,4 +8,10 @@ describe("add", () => {
     it("should return 0 when empty string is passed" , () => {
         expect(add("")).toBe(0);
     });
+
+    it("should return the sum of numbers", () => {
+        expect(add("1")).toBe(1);
+        expect(add("1,2")).toBe(3);
+        expect(add("1,2,3")).toBe(6);
+    });
 });
