29fa24954e04c6ef1164168aaeacd1057b53b7e2
Infant Ajay Venus <infantcsajay@outlook.com>
2024-10-17 00:59:31 +0530

    [index] patch: update add to parse custom delimiter

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 export function add(numbers: string): number {
     if(numbers === "")  return 0;
 
+    const delimiter = numbers.match(/\/\/(.+?)/)?.pop() || ',';
+    
     return numbers
-        .split(/[,\n]/)
+        .replace(/\/\/(.+?)/, '')
+        .replace(new RegExp(delimiter, 'g'), '\n')
+        .split('\n')
         .map((number) => parseInt(number))
         .reduce((sum, number) => sum + number, 0);
 }
