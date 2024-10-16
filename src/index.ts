a04c283e612c786759888ce6be9c0e13493a2530
Infant Ajay Venus <infantcsajay@outlook.com>
2024-10-17 01:24:40 +0530

    [index] patch: modify add to throw error with all negative numbers

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
 export function add(numbers: string): number {
-    if(numbers === "")  return 0;
+    if (numbers === "") return 0;
 
     const delimiter = numbers.match(/\/\/(.+?)/)?.pop() || ',';
-    
-    return numbers
+
+    const parsedNumbers = numbers
         .replace(/\/\/(.+?)/, '')
         .replace(new RegExp(`\\${delimiter}`, 'g'), '\n')
         .split('\n')
         .map((number) => parseInt(number))
-        .reduce((sum, number) => {
-            if(number < 0) {
-                throw new Error(`Negative numbers not allowed ${number}`);
-            }
-            return sum + number;
-        }, 0);
+
+    const negativeNumbers = parsedNumbers.filter((number) => number < 0);
+    if (negativeNumbers.length > 0) {
+        throw new Error(`Negative numbers not allowed ${negativeNumbers.join(',')}`);
+    }
+
+    return parsedNumbers.reduce((sum, number) => {
+        if (number < 0) {
+            throw new Error(`Negative numbers not allowed ${number}`);
+        }
+        return sum + number;
+    }, 0);
 }
