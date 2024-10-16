4391ff3083f0995e830571f0606813d15fc80f29
Infant Ajay Venus <infantcsajay@outlook.com>
2024-10-16 19:58:26 +0530

    [index] patch: update add to accept new line as delimiter

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export function add(numbers: string): number {
     if(numbers === "")  return 0;
 
     return numbers
-        .split(",")
+        .split(/[,\n]/)
         .map((number) => parseInt(number))
         .reduce((sum, number) => sum + number, 0);
 }
